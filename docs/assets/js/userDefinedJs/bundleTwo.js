0c214e2f61fdccaf153586950a4ac3e2b315f7af
Kevin-Estrada <ke81297@gmail.com>
2021-07-07 02:08:59 -0500

    Did some changes to form submission logic such as add a spinner for loading time from request response and added more HTML

--- docs/assets/js/userDefinedJs/bundleTwo.js
+++ docs/assets/js/userDefinedJs/bundleTwo.js
@@ -1,32 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-(function (global){(function (){
-"use strict";
-
-// ref: https://github.com/tc39/proposal-global
-var getGlobal = function () {
-	// the only reliable means to get the global object is
-	// `Function('return this')()`
-	// However, this causes CSP violations in Chrome apps.
-	if (typeof self !== 'undefined') { return self; }
-	if (typeof window !== 'undefined') { return window; }
-	if (typeof global !== 'undefined') { return global; }
-	throw new Error('unable to locate global object');
-}
-
-var global = getGlobal();
-
-module.exports = exports = global.fetch;
-
-// Needed for TypeScript and Webpack.
-if (global.fetch) {
-	exports.default = global.fetch.bind(global);
-}
-
-exports.Headers = global.Headers;
-exports.Request = global.Request;
-exports.Response = global.Response;
-}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}],2:[function(require,module,exports){
 const fetch = require('node-fetch');
 //get the form by its id
 const form = document.querySelector('.contact-form');
@@ -34,14 +6,17 @@ let firstName = document.getElementById('inputFirstName');
 let lastName = document.getElementById('inputLastName');
 let email = document.getElementById('inputEmail');
 let message = document.getElementById('message');
+var element = document.getElementById('spinnerIcon');
+var para = document.getElementById('formSubText');
 
-// const url = 'http://localhost:8080/';
-const url =
-	'https://us-central1-web-portfolio-318903.cloudfunctions.net/sendEmail';
+const url = 'http://localhost:8080/';
+// const url =
+// 	'https://us-central1-web-portfolio-318903.cloudfunctions.net/sendEmail';
 
 //1.
 form.addEventListener('submit', (event) => {
 	event.preventDefault();
+	element.classList.toggle('visually-hidden');
 	main();
 });
 
@@ -75,11 +50,50 @@ async function main() {
 
 	const res = await fetch(url, otherPram);
 	const json = await res.text();
-	console.log(json);
-	alert('Your form was submitted!');
-	form.reset();
+	if (json == 'Success') {
+		element.classList.toggle('visually-hidden');
+		para.innerHTML = 'Your form was successfully submitted! Thank you!';
+		$('#formModal').modal('show');
+		form.reset();
+		console.log(json);
+	} else {
+		element.classList.toggle('visually-hidden');
+		para.innerHTML =
+			'Something went wrong, please try again to resubmit your contact form!';
+		$('#formModal').modal('show');
+		form.reset();
+		console.log(json);
+	}
 }
 
 // main();
 
-},{"node-fetch":1}]},{},[2]);
+},{"node-fetch":2}],2:[function(require,module,exports){
+(function (global){(function (){
+"use strict";
+
+// ref: https://github.com/tc39/proposal-global
+var getGlobal = function () {
+	// the only reliable means to get the global object is
+	// `Function('return this')()`
+	// However, this causes CSP violations in Chrome apps.
+	if (typeof self !== 'undefined') { return self; }
+	if (typeof window !== 'undefined') { return window; }
+	if (typeof global !== 'undefined') { return global; }
+	throw new Error('unable to locate global object');
+}
+
+var global = getGlobal();
+
+module.exports = exports = global.fetch;
+
+// Needed for TypeScript and Webpack.
+if (global.fetch) {
+	exports.default = global.fetch.bind(global);
+}
+
+exports.Headers = global.Headers;
+exports.Request = global.Request;
+exports.Response = global.Response;
+}).call(this)}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{}]},{},[1]);
