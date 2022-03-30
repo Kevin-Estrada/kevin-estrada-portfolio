b7e36c3ffcc5e30e8e6fd2d2f596b4a7a5cb3f14
Kevin-Estrada <56617699+Kevin-Estrada@users.noreply.github.com>
2022-03-30 04:51:26 +0000

    Deploying to gh-pages from @ Kevin-Estrada/kevin-estrada-portfolio@868c859a0070f4f81608d9df01d2d0ab58f91e20 🚀

---
+++ frontend/assets/js/app.js
@@ -0,0 +1,128 @@
+const client = require('./client');
+window.onload = fetchFunction;
+
+const form = document.querySelector('.contact-form');
+let firstName = document.getElementById('inputFirstName');
+let lastName = document.getElementById('inputLastName');
+let email = document.getElementById('inputEmail');
+let message = document.getElementById('message');
+var element = document.getElementById('spinnerIcon');
+var para = document.getElementById('formSubText');
+
+const query =
+  '*[_type == "languageTech" || _type == "framesLibsTech" || _type == "projectsDoc" || _type == "certificatesDoc"] {langLinkToDocs, langAltText, langImage{asset->{_id,url}}, frameLibLinkToDocs, frameLibAltText, frameLibImage{asset->{_id,url}}, NameOfProject, descOfProject, linkToProject, linkToProjectRepo, nameOfCert,issuingOrg,issueDate,certId,certUrl}';
+
+var containerLangTechRowVar = document.getElementById('containerLangTechRow');
+
+var containerFrameLibTechRowVar = document.getElementById(
+  'containerFrameLibTechRow'
+);
+var containerProjectsDocRowVar = document.getElementById(
+  'containerProjectsDocRow'
+);
+
+var carouselIdPlaceVar = document.getElementById('carouselIdPlace');
+
+// const url = 'http://localhost:8080/';
+const url =
+  'https://us-central1-web-portfolio-318903.cloudfunctions.net/sendEmail';
+
+//1.
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  element.classList.toggle('visually-hidden');
+  main();
+});
+
+async function main() {
+  console.log(firstName.value);
+  console.log(lastName.value);
+  console.log(email.value);
+  console.log(message.value);
+
+  const formData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    message: message.value,
+  };
+
+  // let formData = {
+  // 	firstName: 'Kevin',
+  // 	lastName: 'Estrada',
+  // 	email: 'test@email.com',
+  // 	message: 'Hello Sir',
+  // };
+
+  const otherPram = {
+    headers: {
+      'Content-type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(formData),
+  };
+
+  const res = await fetch(url, otherPram);
+  const json = await res.text();
+  if (json == 'Success') {
+    element.classList.toggle('visually-hidden');
+    para.innerHTML = 'Your form was successfully submitted! Thank you!';
+    $('#formModal').modal('show');
+    form.reset();
+    console.log(json);
+  } else {
+    element.classList.toggle('visually-hidden');
+    para.innerHTML =
+      'Something went wrong, please try again to resubmit your contact form!';
+    $('#formModal').modal('show');
+    form.reset();
+    console.log(json);
+  }
+}
+
+async function fetchFunction() {
+  let response = await client.fetch(query);
+  var numCount = 0;
+  for (let x in response) {
+    // console.log(x);
+    // console.log(Object.keys(response[x])[0]);
+
+    if (Object.keys(response[x])[0] == 'langAltText') {
+      var newNodeOne = document.createElement('div');
+      newNodeOne.className = 'col-md-4 col-lg-2';
+      newNodeOne.innerHTML = `<div class="technologies__logo-box shadow-sm"><a href="${response[x].langLinkToDocs}" target="_blank"><img src="${response[x].langImage.asset.url}" alt="${response[x].langAltText}" title="${response[x].langAltText}"class="img-fluid"></a></div>`;
+      containerLangTechRowVar.appendChild(newNodeOne);
+    } else if (Object.keys(response[x])[0] == 'frameLibAltText') {
+      var newNodeTwo = document.createElement('div');
+      newNodeTwo.className = 'col-md-4 col-lg-2';
+      newNodeTwo.innerHTML = `<div class="technologies__logo-box shadow-sm"><a href="${response[x].frameLibLinkToDocs}" target="_blank"><img src="${response[x].frameLibImage.asset.url}" alt="${response[x].frameLibAltText}" title="${response[x].frameLibAltText}"class="img-fluid"></a></div>`;
+      containerFrameLibTechRowVar.appendChild(newNodeTwo);
+    } else if (Object.keys(response[x])[0] == 'NameOfProject') {
+      var newNodeThree = document.createElement('div');
+      newNodeThree.className = 'col-lg-4 col-md-6';
+      if (response[x].linkToProject == 'NA') {
+        newNodeThree.innerHTML = `<div class="card shadow"><div class="card-body"><h5 class="card-title text-decoration-underline">${response[x].NameOfProject}</h5><p class="card-text">${response[x].descOfProject}</p><a href="/docs/pages/project-not-found.html" class="btn btn-primary text-white" style="margin-right: 1rem;">Project Link</a><a href="${response[x].linkToProjectRepo}" target="_blank" class="btn btn-secondary text-white">Repo Link</a></div></div>`;
+      } else {
+        newNodeThree.innerHTML = `<div class="card shadow"><div class="card-body"><h5 class="card-title text-decoration-underline">${response[x].NameOfProject}</h5><p class="card-text">${response[x].descOfProject}</p><a href="${response[x].linkToProject}" target="_blank" class="btn btn-primary text-white" style="margin-right: 1rem;">Project Link</a><a href="${response[x].linkToProjectRepo}" target="_blank" class="btn btn-secondary text-white">Repo Link</a></div></div>`;
+      }
+      containerProjectsDocRowVar.appendChild(newNodeThree);
+    } else {
+      var newNodeFour = document.createElement('div');
+
+      if (numCount == 0) {
+        newNodeFour.className = 'carousel-item active';
+        numCount += 1;
+      } else {
+        newNodeFour.className = 'carousel-item';
+      }
+      newNodeFour.innerHTML = `<div class="certificates__card"><h3>${response[x].nameOfCert}</h3><ul class="certificates__desc"><li>Issuing Organization: ${response[x].issuingOrg}</li><li>Date Issued: ${response[x].issueDate}</li><li>Certificate ID: ${response[x].certId}</li><li><a href="${response[x].certUrl}" target="_blank">See Certificate</a></li></ul></div></div>`;
+      carouselIdPlaceVar.appendChild(newNodeFour);
+    }
+
+    // console.log(x + ': ' + response[x].langImage);
+    // console.log(JSON.stringify(response[x].langImage.asset.url, null, 4));
+    // console.log(x + ': ' + response[x].frameLibImage);
+  }
+}
+
+// main();
