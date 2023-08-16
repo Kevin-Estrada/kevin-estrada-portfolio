4f807ead1d4f8d9aece234f4817ccd0a486c9bae
Kevin Estrda <ke81297@gmail.com>
2023-08-15 23:25:36 -0500

    Adding all the updated details for site

--- frontend/docs/assets/js/client.js
+++ frontend/docs/assets/js/client.js
@@ -1,8 +1,40 @@
-const sanityClient = require('@sanity/client');
-const client = sanityClient({
-  projectId: 'ijui30ya',
-  dataset: 'production',
-  useCdn: true,
-});
+// import { createClient } from '/@sanity/client'
 
-module.exports = client;
+// export const client = createClient({
+//   projectId: 'ijui30ya',
+//   dataset: 'production',
+//   useCdn: true,
+// })
+// // const sanityClient = require('@sanity/client');
+// // const client = sanityClient({
+// //   projectId: 'ijui30ya',
+// //   dataset: 'production',
+// //   useCdn: true,
+// //   apiVersion: '2023-08-14'
+// // });
+
+// export async function getPosts() {
+//   const posts = await client.fetch('*[_type == "languageTech" || _type == "framesLibsTech" || _type == "projectsDoc" || _type == "certificatesDoc"] {langLinkToDocs, langAltText, langImage{asset->{_id,url}}, frameLibLinkToDocs, frameLibAltText, frameLibImage{asset->{_id,url}}, NameOfProject, projectImg{asset->{_id,url}}, descOfProject, linkToProject, linkToProjectRepo, nameOfCert,issuingOrg,issueDate,certId,certUrl}');
+//   return posts;
+// }
+
+
+let PROJECT_ID = "ijui30ya";
+let DATASET = "production";
+let QUERY = encodeURIComponent('*[_type == "languageTech" || _type == "framesLibsTech" || _type == "projectsDoc" || _type == "certificatesDoc"] {langLinkToDocs, langAltText, langImage{asset->{_id,url}}, frameLibLinkToDocs, frameLibAltText, frameLibImage{asset->{_id,url}}, NameOfProject, projectImg{asset->{_id,url}}, descOfProject, linkToProject, linkToProjectRepo, nameOfCert,issuingOrg,issueDate,certId,certUrl}');
+
+// Compose the URL for your project's endpoint and add the query
+let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
+async function clientSanity() {
+  try {
+    const result = await fetch(URL);
+    const results = await result.json();
+    return results;
+  } catch (error) {
+    console.error("An error occurred:", error);
+    return null
+  }
+}
+
+export { clientSanity }
--- sanity-backend/schemas/schema.js
+++ sanity-backend/schemas/schema.js
@@ -1,8 +1,3 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator';
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type';
-
 // We import object and document schemas
 import languageTech from './languageTech';
 import framesLibsTech from './framesLibsTech';
@@ -10,19 +5,9 @@ import projectsDoc from './projectsDoc';
 import certificatesDoc from './certificatesDoc';
 
 // Then we give our schema to the builder and provide the result to Sanity
-export default createSchema({
-	// We name our schema
-	name: 'default',
-	// Then proceed to concatenate our document type
-	// to the ones provided by any plugins that are installed
-	types: schemaTypes.concat([
-		// The following are document types which will appear
-		// in the studio.
-		languageTech,
-		framesLibsTech,
-		projectsDoc,
-		certificatesDoc,
-		// When added to this list, object types can be used as
-		// { type: 'typename' } in other document schemas
-	]),
-});
+export default [
+	languageTech,
+	framesLibsTech,
+	projectsDoc,
+	certificatesDoc,
+]
